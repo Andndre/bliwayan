d08c2b449f0d7edc13c73284673c04ccd2dd4483
andndre <agungandre687@gmail.com>
2024-09-24 21:14:26 +0800

    init landing

---
+++ components/navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/images/logo.png" alt="" height="60">
+    </a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Galerry</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+			<div class="navbar-nav ml-auto">
+				<div class="nav-item">
+					<a class="btn btn-outline-secondary" href="#">Book a Table</a>
+				</div>
+			</div>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bliwayan</title>
+	<!-- Font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<?php include 'components/navbar.php'; ?>
+<main>
+	<!-- Hero - Start -->
+	<section style="background-image: url('images/bg-hero.png'); height: 100dvh; background-repeat: no-repeat; background-size: cover; background-position: center;">
+		<div class="container h-100">
+			<div class="d-flex flex-column gap-2 h-100 justify-content-center align-items-center text-center">
+				<h1 style="max-width: 400px;">Best food for your taste</h1>
+				<p style="max-width: 400px;">Come for the food, and experience with our friendly service.</p>
+				<div class="d-flex gap-2 justify-content-center">
+					<button class="btn btn-primary">
+						Book a Table
+					</button>
+					<button class="btn btn-outline-secondary">
+						Explore Menu
+					</button>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Hero - End -->
+
+	<!-- Browse Our Menu - Start -->
+	<section class="container py-5">
+		<div class="pt-5"></div>
+		<h2 class="text-center">Browse Our Menu</h2>
+		<div class="row pt-4">
+			<div class="col-12 col-md-6 col-lg-3 mt-3">
+				<div class="card">
+					<div class="card-body text-center">
+						<img src="/images/icons/breakfast.svg" alt="breakfast icon" class="w-50" style="max-width: 100px;">
+						<h3 class="pt-4">Breakfast</h3>
+						<p><small>Start your day with our delicious breakfast options, designed to energize and inspire your morning.</small></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3 mt-3">
+				<div class="card">
+					<div class="card-body text-center">
+						<img src="/images/icons/main-dishes.svg" alt="main dishes icon" class="w-50" style="max-width: 100px;">
+						<h3 class="pt-4">Main Dishes</h3>
+						<p><small>Enjoy our main dishes, crafted with fresh ingredients and authentic flavors to satisfy your appetite.</small></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3 mt-3">
+				<div class="card">
+					<div class="card-body text-center">
+						<img src="/images/icons/drinks.svg" alt="drinks icon" class="w-50" style="max-width: 100px;">
+						<h3 class="pt-4">Drinks</h3>
+						<p><small>Refresh yourself with our wide range of beverages, from refreshing cool drinks to warm and soothing options.</small></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3 mt-3">
+				<div class="card">
+					<div class="card-body text-center">
+						<img src="/images/icons/desserts.svg" alt="desserts icon" class="w-50" style="max-width: 100px;">
+						<h3 class="pt-4">Desserts</h3>
+						<p><small>End your meal on a sweet note with our delightful selection of desserts to indulge your taste buds.</small></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="pt-4 text-center">
+			<a class="btn btn-outline-secondary" href="#">Book a Table</a>
+		</div>
+	</section>
+	<!-- Browse Our Menu - End -->
+
+	<!-- We provide healthy food for your family - Start -->
+	 <section class="bg-light">
+			<div class="container py-5">
+				<div class="pt-5"></div>
+				<div class="row">
+					<div class="col-12 col-lg-6">
+						<img src="/images/illustrations/illustration-1.png" alt="illustration">
+					</div>
+					<div class="col-12 col-lg-6">
+						<h2>We provide healthy food for your family.</h2>
+						<p class="pt-3">The story of Bli Wayan Cafe & Kitchen begins with a vision to become a comfortable place to eat, able to satisfy customers, making the taste of the food memorable and hard to forget. Rooted in a rich culinary culture, we strive to honor our local culinary riches while incorporating global flavors.</p>
+						<p class="pt-3">At Bli Wayan Cafe & Kitchen, we believe that dining is not just about the food, but also about the entire experience. Our staff, known for their friendliness and dedication, strives to make every visit a memorable moment.</p>
+						<button class="btn btn-outline-secondary mt-3">More About Us</button>
+					</div>
+				</div>
+			</div>
+	 </section>
+	<!-- We provide healthy food for your family - End -->
+
+	<!-- We also offer optimal services for your experience - Start -->
+	<section class="container py-5">
+		<div class="pt-5"></div>
+		<h2>We also offer optimal services for your experience.</h2>
+		<div class="row pt-5">
+			<div class="col-12 col-md-6 col-lg-3">
+				<img src="/images/illustrations/vip.png" alt="vip">
+				<h3 class="pt-4">VIP</h3>
+				<p>Enjoy exclusive and special service for those seeking a dining experience beyond the ordinary.</p>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<img src="/images/illustrations/bd.png" alt="birthday">
+				<h3 class="pt-4">Birthdays</h3>
+				<p>Celebrate your special day with unforgettable moments, accompanied by delicious food and a warm atmosphere.</p>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<img src="/images/illustrations/ft.png" alt="family time">
+				<h3 class="pt-4">Family Time</h3>
+				<p>Gather your family and spend quality time together while enjoying favorite dishes for everyone.</p>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<img src="/images/illustrations/ho.png" alt="hang out">
+				<h3 class="pt-4">Hang Out</h3>
+				<p>The perfect place to relax and hang out with friends, with a cozy atmosphere and mouth-watering food.</p>
+			</div>
+		</div>
+	</section>
+	<!-- We also offer optimal services for your experience - End -->
+
+</main>
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
